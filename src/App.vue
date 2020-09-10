8360d6016a72e87bea53db216bdd49f9d28e09c2
Kozioleczek <wojciech.koziol@outlook.com>
2020-09-10 12:23:12 +0200

    Added Home scaffolding, added local json file with initial data for Vuex store

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div
+        id="home"
+        class="row mb-3"
+        v-if="step === 0"
+      >
+        <div class="col-12 text-center mb-3">
+          <h1>Palindrom Checker</h1>
+        </div>
+        <div class="col-lg-6 mx-auto col-12 text-left">
+          <div class="card">
+            <div class="card-header">
+              Login in to preceed
+            </div>
+            <div class="card-body">
+            <div class="form-group">
+              <label for="emailInput">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="emailInput"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              >
+            </div>
+            <div class="form-group">
+              <label for="passwordInput">Password</label>
+              <input type="password" class="form-control" id="passwordInput" placeholder="Password">
+            </div>
+            <button
+            class="btn btn-primary w-100 btn-lg"
+            >
+            Login in
+            </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div id="debug" class="row">
+        <div class="col-12">
+          <button @click="get">Init store</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <p class="text-muted">Source code avalible here: <a href="https://github.com/Kozioleczek/coderpeak-palindrom">Github</a></p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import { mapActions } from 'vuex';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      step: 0,
+    };
+  },
+  methods: {
+    ...mapActions(['getInitStore']),
+    get() {
+      this.getInitStore();
+    },
   },
 };
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
+@import 'node_modules/bootstrap/scss/bootstrap';
+@import "node_modules/bootstrap/scss/_functions";
+@import "node_modules/bootstrap/scss/_variables";
+@import "node_modules/bootstrap/scss/mixins/_breakpoints";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Lato', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+const init = require('./initial-store.json');
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    step: 0,
+    data: null,
   },
   mutations: {
+    SET_INIT_STORE(state, data) {
+      this.state.data = data;
+    },
   },
   actions: {
+    getInitStore({ commit }) {
+      console.log(init);
+      commit('SET_INIT_STORE', init);
+    },
   },
   modules: {
   },
